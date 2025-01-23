569947202850f935caaac89852ab0fc4ca02843e
osyra42 <wchiado30@gmail.com>
2025-01-23 15:03:38 -0500

    fixing sync

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import disnake
 from disnake.ext import commands
-import configparser
 import random
 import youtube_dl
 import settings
+import yaml
 
-# Load themes from INI file
-config = configparser.ConfigParser()
-config.read('themes.ini')
+# Load themes from YAML file
+with open('themes.yaml', 'r') as file:
+    themes = yaml.safe_load(file)
 
 # Initialize bot
-bot = commands.Bot(command_prefix='!')
+intents = disnake.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Load cogs
 bot.load_extension('cogs.music')
@@ -19,11 +21,7 @@ bot.load_extension('cogs.music')
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
-
-# Run bot
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user}')
     await bot.sync_commands()
 
+# Run bot
 bot.run(settings.YOUR_BOT_TOKEN)
