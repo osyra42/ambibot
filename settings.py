a61c1d92c69f85e6796970fb8b9d9e75ac1cb356
osyra42 <wchiado30@gmail.com>
2025-02-06 01:13:51 -0500

    removed recorgings

--- settings.py
+++ settings.py
@@ -1,9 +1,7 @@
 
 check_for_updates = True # check for updates on startup
 
-record_time = 600 # in minutes
-
 dir_to_BGM = "sounds/BGM" # directory to BGM
 dir_to_SFX = "sounds/SFX" # directory to SFX
-dir_to_recordings = "recordings" # directory to save recordings
+
 
