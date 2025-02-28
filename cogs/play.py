26bb9797fc49ffe1eac882d1da402f4cff67b4c2
Kastien Dev <katarebornmc@gmail.com>
2025-02-28 13:29:19 -0500

    loc updatev2

--- cogs/play.py
+++ cogs/play.py
@@ -20,6 +20,7 @@ ytdl_format_options = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
 }
 
 ffmpeg_options = {
